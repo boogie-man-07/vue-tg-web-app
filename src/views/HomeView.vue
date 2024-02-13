55d5d43a351962cc5900489a5958072a209a8c28
boogie-man-07 <booogie.man.07@gmail.com>
2024-02-13 23:57:25 +0300

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
-<script setup>
+<script>
+import { useTelegram } from '@/useTelegram';
 import HomeComponent from '../components/HomeComponent.vue'
+
+
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    const tg = useTelegram()
+    tg.expand()
+  }
+}
+
 </script>
 
 <template>
