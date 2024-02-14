83514dc6ead21d71096bba40e193d5950e51edb4
boogie-man-07 <booogie.man.07@gmail.com>
2024-02-14 21:45:41 +0300

    Tg main button logic, item selection and style added.

--- src/components/EmployeeItemComponent.vue
+++ src/components/EmployeeItemComponent.vue
@@ -1,27 +1,29 @@
 <template>
-    <el-col
-      class="request-item" shadow="hover" 
+    <el-card
+      class="request-item" :class="employee.isSelected ? 'selected-card' : 'deselected-card'" shadow="hover" 
       @click="chooseEmployee(employee)"
     >
-        <el-row class="card-padding">
-            <el-col>
-                <el-text class="title-text w-100" truncated> ФИО: {{ employee.fullname }}</el-text>
-                <el-text class="subtitle-text w-100" truncated>Должность: {{ employee.position }}</el-text>
-            </el-col>
-        </el-row>
-    </el-col>
+    <el-row>
+        <el-col>
+            <el-text class="title-text w-100" truncated> ФИО: {{ employee.fullname }}</el-text>
+            <el-text class="subtitle-text w-100" truncated>Должность: {{ employee.position }}</el-text>
+        </el-col>
+    </el-row>
+    </el-card>
 </template>
   
 <script>
   
 export default {
     name: "EmployeeItemComponent",
+    emits: ['select-item'],
     components: {},
     props: {
         employee: Object
     },
     methods: {
         chooseEmployee(item) {
+            this.$emit('select-item', item.id)
             console.log(item)
         }
     },
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -4,11 +4,21 @@
             <el-input class="custom-input" v-model="filteredValue" type="text" placeholder="Поиск работника" clearable />
         </el-col>
     </el-row>
-    <el-row class="item-card">
-        <template v-for="employee in employeesList" :key="employee.id">
-            <employee-item-component :employee="employee" />
-        </template>
-    </el-row>
+    <template v-if="employeesList.length > 0  && filteredValue != ''">
+        <el-row>
+            <template v-for="employee in employeesList" :key="employee.id">
+                <employee-item-component :employee="employee" @select-item="selectItem" />
+            </template>
+        </el-row>
+    </template>
+    <template v-if="filteredValue == ''">
+        <el-row class="empty-card">
+            <el-col>
+                <el-text class="empty-card-text"><h2>Введите ФИО работника для поиска</h2></el-text>
+                <el-text class="empty-card-text"><h5>Как только вы начнете вводить ФИО, работники подходящие под условия поиска высветятся в результатах поисках. Выделите необходимого работника и нажмите "Получить карточку".</h5></el-text>
+            </el-col>
+        </el-row>
+    </template>
 </template>
 
 <script>
@@ -23,11 +33,33 @@ export default {
         return {
             filteredValue: '',
             employees: [
-                { id: 1, fullname: 'Адыгезалов Мурад Арифович', position: 'руководитель проектов' },
-                { id: 2, fullname: 'Иванов Дмитрий Сергеевич', position: 'рукодводитель группы развития' },
-                { id: 3, fullname: 'Мальцев Сергей Владимирович', position: 'руководитель ИТ департамента' },
+                { id: 1, fullname: 'Адыгезалов Мурад Арифович', position: 'руководитель проектов', isSelected: false },
+                { id: 2, fullname: 'Иванов Дмитрий Сергеевич', position: 'рукодводитель группы развития', isSelected: false },
+                { id: 3, fullname: 'Мальцев Сергей Владимирович', position: 'руководитель ИТ департамента', isSelected: false },
             ],
         }
+    },
+    watch: {
+        isAnySelected() {
+            if (this.isAnySelected) {
+                tg.MainButton.show()
+            } else {
+                tg.MainButton.hide()
+            }
+        },
+    },
+    mounted() {
+        const tg = window.Telegram.WebApp
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    },
+    methods: {
+        selectItem(id) {
+            this.employees.forEach(el => {
+                el.id == id ? el.isSelected = !el.isSelected : el.isSelected = false
+            });
+        },
     },
     computed: {
         employeesList() {
@@ -36,6 +68,9 @@ export default {
                     el.fullname.toLowerCase().includes(this.filteredValue.toLowerCase()
                 )
             )
+        },
+        isAnySelected() {
+            return this.employees.filter(el => el.isSelected == true).length > 0
         },
     },
 }
