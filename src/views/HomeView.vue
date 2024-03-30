ce81cf6a0f5ec0922ada4a1f95cdb46c0522b764
boogie-man-07 <booogie.man.07@gmail.com>
2024-03-30 15:56:13 +0300

    Removed mock from chatId.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,7 +28,7 @@ export default {
     tg.MainButton.onClick(this.send)
 
     tg.expand()
-    this.chatId = tg.initDataUnsafe?.chat.id
+    this.chatId = tg.initDataUnsafe?.user.chat.id
   },
   methods: {
     changeTgButtonState(state) {
