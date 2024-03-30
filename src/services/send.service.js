48f48a9616ee6cc49857614d26e44e6cdec88baa
boogie-man-07 <booogie.man.07@gmail.com>
2024-03-30 15:15:42 +0300

    Removed mock from chatId.

--- src/services/send.service.js
+++ src/services/send.service.js
@@ -2,15 +2,6 @@ import axios from 'axios';
 
 export default {
     sendCard(chatId, cardId) {
-        const headers = {
-            "Content-Type": "application/json"
-        };
-
-        const body = JSON.stringify({
-            chatId: chatId,
-            cardId: cardId
-        });
-
-        axios.post('https://sigmabot.ddns.net/test/bot-get-card.php', body, { headers: headers });
+        axios.get(`https://sigmabot.ddns.net/test/bot-get-card.php?chatId=${chatId}&cardId=${cardId}`);
     }
 }
