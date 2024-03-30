8d6e3374c1b7f7cb83bd556ce5b02962b31aec7c
boogie-man-07 <booogie.man.07@gmail.com>
2024-03-30 16:17:04 +0300

    Handle chatData object.

--- src/services/send.service.js
+++ src/services/send.service.js
@@ -2,15 +2,11 @@ import axios from 'axios';
 
 export default {
     sendCard(chatData, cardId) {
-        const headers = {
-            "Content-Type": "application/json"
-        };
-
         const body = JSON.stringify({
             chatData: chatData,
             cardId: cardId
         });
 
-        axios.post('https://sigmabot.ddns.net/test/bot-get-card.php', body, { headers: headers });
+        axios.post('https://sigmabot.ddns.net/test/bot-get-card.php', body);
     }
 }
